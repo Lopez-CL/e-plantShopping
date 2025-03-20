b9e2323a027cec71f12afbc86a0d22f975aae842
Lopez-CL <cllopez511@gmail.com>
2025-03-20 18:00:12 -0400

    completed tasks

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,15 +7,29 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
-    
+      const existingPlant = state.items.find(plant => plant.name === action.payload.name)
+      if(existingPlant){
+        existingPlant.quantity+= 1;
+      }else{
+        state.items.push({...action.payload, quantity : 1})
+      }
     },
     removeItem: (state, action) => {
+      const updatedList = state.items.filter(plant=>(
+        plant.name !== action.payload.name
+      ))
+      state.items = updatedList
     },
     updateQuantity: (state, action) => {
-
-    
-    },
-  },
+      const {name, quantity} = action.payload;
+      const itemToUpdate = state.items.find(plant =>(
+        plant.name === name
+      ))
+      if( itemToUpdate.quantity + quantity < 0){
+      return
+      }
+      itemToUpdate.quantity = quantity;
+  }}
 });
 
 export const { addItem, removeItem, updateQuantity } = CartSlice.actions;
